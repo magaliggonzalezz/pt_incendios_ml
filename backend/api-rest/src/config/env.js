5c0c0daf5d43059a8ab4cdaa5e575aeb128daa98
Magali Gonzalez <mgonzalezg1604@alumno.ipn.mx>
2026-08-14 20:45:09 -0600

    feat: agrega url base de assets externos

--- backend/api-rest/src/config/env.js
+++ backend/api-rest/src/config/env.js
@@ -6,6 +6,7 @@ export const env = {
   port: process.env.PORT || 3000,
   mongoUri: process.env.MONGODB_URI,
   mongoDbName: process.env.MONGODB_DB_NAME || "incendios_forestales_v2",
+  assetsBaseUrl: process.env.ASSETS_BASE_URL,
   msRecoleccionUrl: process.env.MS_RECOLECCION_URL,
   msPreprocesamientoUrl: process.env.MS_PREPROCESAMIENTO_URL,
   msAnalisisMlUrl: process.env.MS_ANALISIS_ML_URL,
