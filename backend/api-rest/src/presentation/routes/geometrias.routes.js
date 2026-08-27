6ea07f14f38daa27f5ee7c1a567656fa34398bc4
Magali Gonzalez <mgonzalezg1604@alumno.ipn.mx>
2026-08-26 20:53:55 -0600

    feat: agrega ruta SMN para capas propias

--- backend/api-rest/src/presentation/routes/geometrias.routes.js
+++ backend/api-rest/src/presentation/routes/geometrias.routes.js
@@ -6,5 +6,6 @@ const controller = new GeometriasController();
 
 router.get("/estados", controller.estados.bind(controller));
 router.get("/municipios", controller.municipios.bind(controller));
+router.get("/smn", controller.smn.bind(controller));
 
 export default router;
